5dfb74123237d8a4e2eac9c9d8dca05bf3f81d21
Loruel <paco.jeansl@gmail.com>
2024-08-28 12:48:06 -0600

    correccion botton antes

--- src/components/OnePokemon/anteriorPoke.jsx
+++ src/components/OnePokemon/anteriorPoke.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import { usePoke } from '../../PokeContext'
-import { useNavigate } from 'react-router'
+
 
 
 export default function AnteriorPoke() {
-    const { getPreviousPokemon } = usePoke()
-    const navegacion = useNavigate()
-
-    const handleClick = () => {
-        const prevPoke = getPreviousPokemon(currentIndex);
-        navigate(`/pokemon/${prevPoke.Numero}`)
-    };
-
-    const prevPoke = getPreviousPokemon(currentIndex);
-
+    
     return (
         <div>
             <button className='text-white font-bold flex items-center justify-start'>
